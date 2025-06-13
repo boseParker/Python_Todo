ab54022591c4a68465523326cea8612bd31d2524
Bose <bosem3708@gmail.com>
2025-06-13 11:53:26 +0530

    gunicorn install

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,6 +124,13 @@ STATICFILES_DIRS=[
 ]
 
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
